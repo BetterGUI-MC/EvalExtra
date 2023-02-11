02e938b6645d937fc6986f294076e475c8fef19c
HSGamer <huynhqtienvtag@gmail.com>
2023-02-11 17:19:38 +0700

    use my fork of EvalEx

--- src/main/java/me/hsgamer/bettergui/evalextra/EvalExtra.java
+++ src/main/java/me/hsgamer/bettergui/evalextra/EvalExtra.java
@@ -1,15 +1,20 @@
 package me.hsgamer.bettergui.evalextra;
 
+import com.ezylang.evalex.Expression;
+import com.ezylang.evalex.config.ExpressionConfiguration;
 import me.hsgamer.bettergui.util.StringReplacerApplier;
 import me.hsgamer.hscore.bukkit.addon.PluginAddon;
 import me.hsgamer.hscore.expression.ExpressionUtils;
 
-import java.math.BigDecimal;
-import java.util.Optional;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public final class EvalExtra extends PluginAddon {
+    private final ExpressionConfiguration configuration = ExpressionConfiguration.builder()
+            .implicitMultiplicationAllowed(false)
+            .structuresAllowed(false)
+            .arraysAllowed(false)
+            .build();
     private final Pattern skipPattern = Pattern.compile("\\[skip-eval]\\s?(.*)", Pattern.CASE_INSENSITIVE);
 
     @Override
@@ -19,7 +24,12 @@ public final class EvalExtra extends PluginAddon {
             if (matcher.find()) {
                 return matcher.group(1);
             }
-            return Optional.ofNullable(ExpressionUtils.getResult(original)).map(BigDecimal::toPlainString).orElse(original);
+            try {
+                Expression expression = ExpressionUtils.createExpression(original, () -> configuration);
+                return expression.evaluate().getStringValue();
+            } catch (Exception e) {
+                return original;
+            }
         });
     }
 }
