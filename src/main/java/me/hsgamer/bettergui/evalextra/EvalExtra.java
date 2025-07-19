0c4741c608333e7f752445e89c7e87a8b1023778
hsgamer <huynhqtienvtag@gmail.com>
2025-07-19 20:28:14 +0700

    add a variable

--- src/main/java/me/hsgamer/bettergui/evalextra/EvalExtra.java
+++ src/main/java/me/hsgamer/bettergui/evalextra/EvalExtra.java
@@ -5,6 +5,7 @@ import com.ezylang.evalex.config.ExpressionConfiguration;
 import me.hsgamer.bettergui.util.StringReplacerApplier;
 import me.hsgamer.hscore.expansion.common.Expansion;
 import me.hsgamer.hscore.expression.ExpressionUtils;
+import me.hsgamer.hscore.variable.VariableBundle;
 
 import java.util.Map;
 import java.util.Set;
@@ -19,28 +20,37 @@ public final class EvalExtra implements Expansion {
                     .arraysAllowed(false)
                     .build()
     );
-    private final String skipEvalString = "[skip-eval]";
     private final Map<String, Expression> cachedExpressionMap = new ConcurrentHashMap<>();
     private final Set<String> cachedStaticStringMap = new ConcurrentSkipListSet<>();
+    private final VariableBundle variableBundle = new VariableBundle();
+
+    private String evaluate(String expression) {
+        String skipEvalString = "[skip-eval]";
+        if (expression.startsWith(skipEvalString)) {
+            return expression.substring(skipEvalString.length());
+        }
+
+        if (cachedStaticStringMap.contains(expression)) {
+            return expression;
+        }
+
+        Expression eval = cachedExpressionMap.computeIfAbsent(expression, key -> new Expression(expression, configuration));
+        try {
+            return eval.evaluate().getStringValue();
+        } catch (Exception e) {
+            cachedStaticStringMap.add(expression);
+            return expression;
+        }
+    }
 
     @Override
     public void onEnable() {
-        StringReplacerApplier.getStringReplacers().add((original) -> {
-            if (original.startsWith(skipEvalString)) {
-                return original.substring(skipEvalString.length());
-            }
+        StringReplacerApplier.getStringReplacers().add(this::evaluate);
+        variableBundle.register("eval_", this::evaluate);
+    }
 
-            if (cachedStaticStringMap.contains(original)) {
-                return original;
-            }
-
-            Expression expression = cachedExpressionMap.computeIfAbsent(original, key -> new Expression(original, configuration));
-            try {
-                return expression.evaluate().getStringValue();
-            } catch (Exception e) {
-                cachedStaticStringMap.add(original);
-                return original;
-            }
-        });
+    @Override
+    public void onDisable() {
+        variableBundle.unregisterAll();
     }
 }
